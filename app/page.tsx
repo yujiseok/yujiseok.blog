f3dd2d6083ffe5a2b5fc20b15a98b63bf4fa5245
yujiseok <koesijuy@gmail.com>
2023-02-21 01:20:47 +0900

    Feat: home 페이지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+import { name, avatar, intro, bottomIntro } from "lib/home";
+
 export default function Home() {
-  return <h1 className="">Hello world!</h1>;
+  return (
+    <section>
+      <div className="my-4 flex items-center gap-8">
+        <div className="flex h-[125px] w-[125px]">
+          <Image
+            src={avatar}
+            width={125}
+            height={125}
+            alt={name}
+            placeholder="blur"
+            priority
+            className="h-full w-full rounded-full object-cover"
+          />
+        </div>
+        {intro()}
+      </div>
+      {bottomIntro()}
+    </section>
+  );
 }
