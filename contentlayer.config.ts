9b4ad2cccd33cd81217777173d34bed85a0367b6
yujiseok <koesijuy@gmail.com>
2023-02-20 23:36:24 +0900

    Feat: contentlayer 설정

---
+++ contentlayer.config.ts
@@ -0,0 +1,65 @@
+import type { ComputedFields } from "contentlayer/source-files";
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import readingTime from "reading-time";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
+import rehypePrettyCode from "rehype-pretty-code";
+
+/** @type {import('contentlayer/source-files').ComputedFields} */
+const computedFields: ComputedFields = {
+  readingTime: { type: "json", resolve: (doc) => readingTime(doc.body.raw) },
+  slug: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath,
+  },
+};
+
+export const Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: `**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    publishedAt: {
+      type: "string",
+      required: true,
+    },
+    summary: {
+      type: "string",
+      required: true,
+    },
+    image: {
+      type: "string",
+    },
+  },
+  computedFields,
+}));
+
+export default makeSource({
+  contentDirPath: "content",
+  documentTypes: [Blog],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "material-theme-palenight",
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"],
+          },
+        },
+      ],
+    ],
+  },
+});
