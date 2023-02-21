e6b5392f722d51664578b7a0f646df7068f5566c
yujiseok <koesijuy@gmail.com>
2023-02-21 13:14:24 +0900

    Feat: lo-fi 블로그 ui, 테스트 추가

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,71 @@
-const Blog = () => {
-  return <div>individual blog</div>;
+import type { Metadata } from "next";
+import { allBlogs } from "contentlayer/generated";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import Balancer from "react-wrap-balancer";
+// export const metadata: Metadata = {
+//   title: "Blog",
+//   description: "공유하고 싶은 생각 또는 기술들을 소개합니다.",
+// };
+
+export async function generateStaticParams() {
+  return allBlogs.map((blog) => ({
+    slug: blog.slug,
+  }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata | undefined> {
+  const blog = allBlogs.find((blog) => blog.slug === params.slug);
+  if (!blog) {
+    return;
+  }
+  const {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+    slug,
+  } = blog;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title: { absolute: title },
+      description,
+      type: "article",
+      publishedTime,
+      url: `https://www.yujiseok.blog/blog/${slug}`,
+    },
+  };
+}
+
+const Blog = ({ params }: { params: { slug: string } }) => {
+  const blog = allBlogs.find((blog) => blog.slug === params.slug);
+  if (!blog) {
+    return false;
+  }
+  const MDXContent = useMDXComponent(blog.body.code);
+
+  return (
+    <section>
+      <div className="mb-6">
+        <h1 className="text-3xl font-semibold">
+          <Balancer>{blog.title}</Balancer>
+        </h1>
+        <h4 className="">{blog.summary}</h4>
+        <p>
+          <small>{blog.publishedAt}</small>{" "}
+          <small>{blog.readingTime.text}</small>
+        </p>
+      </div>
+
+      <article className="prose break-keep dark:prose-invert prose-h1:text-2xl">
+        <MDXContent />
+      </article>
+    </section>
+  );
 };
 export default Blog;
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,36 @@
 import type { Metadata } from "next";
+import { allBlogs } from "contentlayer/generated";
+import Link from "next/link";
+
 export const metadata: Metadata = {
   title: "Blog",
-  description: "",
+  description: "공유하고 싶은 생각 또는 기술들을 소개합니다.",
 };
 
 function BlogPage() {
-  return <div>Blog Page</div>;
+  return (
+    <section>
+      <h1 className="mb-5 text-3xl font-bold">Blog</h1>
+
+      {allBlogs
+        .sort((a, b) => {
+          if (new Date(a.publishedAt) > new Date(b.publishedAt)) return -1;
+
+          return 1;
+        })
+        .map((blog) => (
+          <article key={blog.slug} className="mb-4">
+            <Link href={`/blog/${blog.slug}`}>
+              <h3 className="text-xl font-semibold">{blog.title}</h3>
+              <h6 className="font-light text-gray-500">{blog.summary}</h6>
+              <p>
+                <small className="mr-2">{blog.publishedAt}</small>
+                <small>{blog.readingTime.text}</small>
+              </p>
+            </Link>
+          </article>
+        ))}
+    </section>
+  );
 }
 export default BlogPage;
