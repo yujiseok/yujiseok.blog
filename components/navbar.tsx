a7007fa6d478c87fedf117e673326479fc0ac3f2
yujiseok <koesijuy@gmail.com>
2023-02-21 13:13:58 +0900

    Feat: 스타일 수정, pathname에 스타일

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,7 @@
-import { HiOutlineBars3 } from "react-icons/hi2";
+"use client";
 import Link from "next/link";
 import DarkModeBtn from "./darkModeBtn";
+import { usePathname } from "next/navigation";
 
 interface NavItem {
   name: "home" | "about" | "blog";
@@ -23,15 +24,20 @@ const NAV_ITEMS: NavItem[] = [
 ];
 
 function Navbar() {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
-    <nav className="fixed top-0 z-10 h-14 w-full">
+    <nav className="mt-12 h-14 w-full">
       <div className="mx-auto flex h-full w-full max-w-2xl items-center justify-between px-4 py-4">
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-3xl font-bold">
           <Link href="/">Yujiseok</Link>
         </h1>
         <ul className="flex items-center gap-4">
           {NAV_ITEMS.map(({ name, href }) => (
-            <li key={name}>
+            <li
+              key={name}
+              className={`${pathname?.includes(name) ? "underline" : ""} `}
+            >
               <Link href={href}>{name}</Link>
             </li>
           ))}
