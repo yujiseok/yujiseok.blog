7099da5ec2cadb62a997ba4dd84009ad82684794
yujiseok <koesijuy@gmail.com>
2023-02-22 12:33:30 +0900

    Feat: footer 보더 삭제

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="mx-auto mt-auto flex w-full max-w-2xl items-center px-4">
-      <div className="mt-6 flex w-full flex-col items-center justify-center gap-4 border-t py-6">
+      <div className="mt-6 flex w-full flex-col items-center justify-center gap-4 py-6">
         <p>© {new Date().getFullYear()} 유지석 All Rights Reserved.</p>
         <a
           href="https://github.com/yujiseok"
