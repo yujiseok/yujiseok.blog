bef04d1f0de28ab64e2207824b8d2e28fad69431
yujiseok <koesijuy@gmail.com>
2023-03-12 21:42:26 +0900

    Refactor: lib/home.ts 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { name, avatar, intro, bottomIntro } from "lib/home";
+import { name, avatar } from "lib/home";
 
 export default function Home() {
   return (
---
+++ lib/home.ts
@@ -0,0 +1,4 @@
+import me from "../app/avatar.jpg";
+
+export const name = "유지석";
+export const avatar = me;
