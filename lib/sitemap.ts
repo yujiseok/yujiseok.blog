190fad5f4f9667365ddcb1a39520eb13ec2ba747
yujiseok <koesijuy@gmail.com>
2023-03-25 02:07:45 +0900

    Feat: 불필요 콘솔 제거

--- lib/sitemap.ts
+++ lib/sitemap.ts
@@ -17,8 +17,6 @@ export function createSiteMap() {
     )
     .join("");
 
-  console.log(postUrls);
-
   const generatedSitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
