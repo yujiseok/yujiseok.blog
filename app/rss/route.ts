85f08b15261bfb36ad3275e7e209e3f53d8734e3
yujiseok <koesijuy@gmail.com>
2024-06-22 00:19:00 +0900

    feat: rss feed 라우트 생성

---
+++ app/rss/route.ts
@@ -0,0 +1,34 @@
+import { baseURL } from "../sitemap";
+import { getAllPosts } from "@/lib/utils";
+
+export async function GET() {
+  const posts = getAllPosts();
+
+  const XML = posts
+    .map(
+      (post) =>
+        `<item>
+          <title>${post.metadata.title}</title>
+          <link>${baseURL}/post/${post.slug}</link>
+          <description>${post.metadata.summary}</description>
+          <pubDate>${new Date(post.metadata.publishedAt).toUTCString()}</pubDate>
+        </item>`,
+    )
+    .join("\n");
+
+  const RSS = `<?xml version="1.0" encoding="UTF-8" ?>
+   <rss version="2.0">
+      <channel>
+        <title>Yujiseok's Blog</title>
+        <link>${baseURL}</link>
+        <description>This is my blog RSS feed</description>
+        ${XML}
+      </channel>
+    </rss>`;
+
+  return new Response(RSS, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
