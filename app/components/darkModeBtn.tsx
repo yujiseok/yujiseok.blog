fada0fe3a511ec9dcc67e19fa8650adcc90c3377
yujiseok <koesijuy@gmail.com>
2024-10-27 18:23:07 +0900

    feat: 하이드레이션 미스매치 오류 수정

--- app/components/darkModeBtn.tsx
+++ app/components/darkModeBtn.tsx
@@ -1,19 +1,13 @@
 import { HiOutlineSun, HiOutlineMoon } from "react-icons/hi2";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 function DarkModeBtn() {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
 
   return (
     <>
-      {mounted && currentTheme === "dark" ? (
+      {currentTheme === "dark" ? (
         <button
           type="button"
           title="Toggle dark mode"
--- app/components/provider.tsx
+++ app/components/provider.tsx
@@ -1,7 +1,14 @@
 "use client";
 import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 function ThemeProviders({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       {children}
