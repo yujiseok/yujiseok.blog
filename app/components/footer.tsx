d2506c00b01457e9b9c8384011c8e487b547565e
yujiseok <koesijuy@gmail.com>
2025-04-01 21:46:25 +0900

    feat: footer 수정

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,21 +1,13 @@
+import Link from "next/link";
 import DarkModeBtn from "./darkModeBtn";
 
 function Footer() {
   return (
-    <footer className="mx-auto mt-auto flex w-full max-w-2xl items-center px-4">
-      <div className="mt-10 flex w-full flex-col items-center justify-center gap-4 py-6">
-        <p>© {new Date().getFullYear()} 유지석 All Rights Reserved.</p>
+    <footer className="mx-auto mt-auto flex w-full max-w-2xl items-center px-4 text-neutral-600 md:text-sm dark:text-neutral-400">
+      <div className="mt-10 flex w-full justify-between gap-4 py-6">
+        <p>© {new Date().getFullYear()} 유지석</p>
         <div className="flex items-center gap-4">
-          <a
-            href="https://github.com/yujiseok"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-          <a href="mailto:koesijuy@gmail.com" rel="noopener noreferrer">
-            Contact
-          </a>
+          <Link href="/resume">Resume</Link>
           <DarkModeBtn />
         </div>
       </div>
