87429f3fb5df5a68670473d8bee4f47ad0ecdecd
yujiseok <koesijuy@gmail.com>
2025-04-02 00:39:48 +0900

    feat: 레이아웃 수정

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,8 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="mx-auto mt-28 w-full max-w-2xl px-4">{children}</main>
+    <main className="mx-auto mt-24 w-full max-w-2xl px-4 sm:mt-28">
+      {children}
+    </main>
   );
 };
 export default Layout;
--- app/components/arrowUpRight.tsx
+++ app/components/arrowUpRight.tsx
@@ -1,18 +1,19 @@
 const ArrowUpRight = () => {
   return (
     <svg
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1"
-      stroke="currentColor"
-      className="size-3.5 transition-transform duration-200 ease-out will-change-transform group-hover:translate-x-0.5"
+      className="transition-transform duration-200 ease-out will-change-transform group-hover:translate-x-0.5"
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-      />
+        d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      ></path>
     </svg>
   );
 };
