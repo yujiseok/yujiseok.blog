a0aa5a606099e91063d8e89d3f4bd35fb615c01f
yujiseok <koesijuy@gmail.com>
2025-04-01 21:47:23 +0900

    feat: atelier 페이지 추가

---
+++ app/(main)/atelier/page.tsx
@@ -0,0 +1,10 @@
+import BlurContainer from "@/app/components/blurContainer";
+
+const page = () => {
+  return (
+    <BlurContainer>
+      <div>Cooking</div>
+    </BlurContainer>
+  );
+};
+export default page;
