0658c70450ad3b8d2235d3bb620bbc78e4fa0d7f
yujiseok <koesijuy@gmail.com>
2025-04-01 21:47:08 +0900

    feat: resume 페이지 추가

---
+++ app/(main)/resume/page.tsx
@@ -0,0 +1,40 @@
+import BlurContainer from "@/app/components/blurContainer";
+
+const page = () => {
+  return (
+    <BlurContainer>
+      <div>Cooking</div>
+      {/* <div>
+        <h4 className="font-semibold">Career</h4>
+        <ul className="flex list-disc flex-col gap-1 px-4">
+          <li>
+            <div>
+              <p>데이원컴퍼니 (2023.11 ~ )</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4 className="font-semibold">Education</h4>
+        <ul className="flex list-disc flex-col gap-1 px-4">
+          <li>
+            패스트캠퍼스 메가바이트 스쿨 프론트엔드 과정 (2022.09.13 -
+            2023.04.14)
+          </li>
+          <li>원티드 프리온보딩 프론트엔드 인턴십 (2023.02.20 - 2023.03.17)</li>
+        </ul>
+      </div>
+      <div>
+        <h4 className="font-semibold">Open Source Contributions</h4>
+        <ul className="flex list-disc flex-col gap-1 px-4">
+          <li>
+            <div>
+              <p>리액트 공식 문서 코드 오류 수정</p>
+            </div>
+          </li>
+        </ul>
+      </div> */}
+    </BlurContainer>
+  );
+};
+export default page;
