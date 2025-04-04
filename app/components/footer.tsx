ae0893608ab9663347ea4833a79b28b84483be7b
yujiseok <koesijuy@gmail.com>
2025-04-04 20:44:19 +0900

    feat: footer 링크 추가 및 텍스트 수정

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="mx-auto mt-auto flex w-full max-w-2xl items-center text-neutral-600 md:text-sm dark:text-neutral-400">
       <div className="mt-10 flex w-full justify-between gap-4 py-6">
-        <p>© {new Date().getFullYear()} 유지석</p>
+        <Link href="/">© {new Date().getFullYear()} 유지석</Link>
         <div className="flex items-center gap-4">
           <Link href="/resume">Resume</Link>
           <DarkModeBtn />
