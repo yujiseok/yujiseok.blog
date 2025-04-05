d589922ced4ed0006a1d99ae64d51953597b6a72
yujiseok <koesijuy@gmail.com>
2025-04-05 17:52:54 +0900

    feat: 네비게이션을 위한 FixedBackLink 추가

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,11 @@
+import FixedBackLink from "../components/fixedBackLink";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="mx-auto mt-24 w-full max-w-2xl sm:mt-28">{children}</main>
+    <main className="mx-auto mt-24 w-full max-w-2xl sm:mt-28">
+      <FixedBackLink />
+      {children}
+    </main>
   );
 };
 export default Layout;
---
+++ app/components/fixedBackLink.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import type { SVGProps } from "react";
+
+const FixedBackLink = () => {
+  const pathname = usePathname();
+  const isWritingIndex = pathname === "/writing";
+
+  if (!pathname.startsWith("/writing")) return null;
+
+  const href = isWritingIndex ? "/" : "/writing";
+
+  return (
+    <nav
+      className="fadeIn pointer-events-none inset-x-0 top-24 z-50 lg:fixed lg:top-28"
+      aria-label="navigation"
+    >
+      <div className="mb-4 -ml-3 max-w-4xl lg:mx-auto lg:px-3.5">
+        <Link
+          href={href}
+          className="group pointer-events-auto flex w-fit items-center gap-1.5 text-sm text-neutral-600 dark:text-neutral-400"
+          aria-label="back"
+        >
+          <ArrowUTurnLeft aria-hidden="true" role="presentation" />
+          <span>뒤로 가기</span>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default FixedBackLink;
+
+const ArrowUTurnLeft = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="mb-0.5 size-3.5 transition-transform duration-200 ease-out will-change-transform group-hover:-translate-x-0.5"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+    />
+  </svg>
+);
