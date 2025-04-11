475158e32f38fc361336908c6d78fddac7cda7a8
yujiseok <koesijuy@gmail.com>
2025-04-11 21:30:05 +0900

    feat: sitemap 함수 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,15 +4,15 @@ import type { MetadataRoute } from "next";
 export const baseURL = "https://www.yujiseok.blog";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const posts = getAllWritings().map((post) => ({
-    url: `https://www.yujiseok.blog/post/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+  const writings = getAllWritings().map((writing) => ({
+    url: `${baseURL}/writing/${writing.slug}`,
+    lastModified: writing.metadata.publishedAt,
   }));
 
-  const routes = ["", "/post", "/about"].map((route) => ({
-    url: `https://www.yujiseok.blog${route}`,
+  const routes = ["", "/writing"].map((route) => ({
+    url: `${baseURL}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...posts];
+  return [...routes, ...writings];
 }
