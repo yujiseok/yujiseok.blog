d272d52ed4b7c664ec7a27ffa42b8614cb74fb0c
yujiseok <koesijuy@gmail.com>
2025-04-15 22:59:00 +0900

    feat: GridItem 컴포넌트 추가

---
+++ app/components/atelier/gridItem.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { Metadata } from "@/lib/utils";
+import Image from "next/image";
+
+const GridItem = ({ metadata, slug }: { metadata: Metadata; slug: string }) => {
+  const { title, image = "" } = metadata;
+
+  return (
+    <Link
+      href={`/atelier/${slug}`}
+      className="rounded-lg border border-neutral-300/80 p-px dark:border-neutral-700"
+    >
+      <div className="grid gap-2 p-1">
+        <Image
+          src={image}
+          alt={title}
+          width={320}
+          height={180}
+          quality={100}
+          className="size-full object-cover"
+        />
+        <h3 className="text-sm font-medium">{title}</h3>
+      </div>
+    </Link>
+  );
+};
+
+export default GridItem;
