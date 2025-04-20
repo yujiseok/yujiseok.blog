8a6acf3e53f7fc3403d2773bfecdaad6c65bc2b9
yujiseok <koesijuy@gmail.com>
2025-04-20 12:43:47 +0900

    feat: ShowCase 컴포넌트에 rerun 기능 추가 및 상태 관리 구현

--- app/components/atelier/showCase.tsx
+++ app/components/atelier/showCase.tsx
@@ -1,8 +1,27 @@
-const ShowCase = ({ children }: { children: React.ReactNode }) => {
+"use client";
+
+import Rerun from "./rerun";
+import React, { useState } from "react";
+
+const ShowCase = ({
+  children,
+  rerun = false,
+}: {
+  children: React.ReactNode;
+  rerun?: boolean;
+}) => {
+  const [key, setKey] = useState(0);
+
+  const handleRerun = () => {
+    setKey((prev) => prev + 1);
+  };
+
   return (
     <div className="relative grid h-96 place-items-center overflow-hidden rounded-lg border border-neutral-300/80 bg-neutral-100/50 p-0.5 dark:border-neutral-700 dark:bg-neutral-900/50">
-      {children}
+      {rerun && <Rerun handleRerun={handleRerun} />}
+      <React.Fragment key={key}>{children}</React.Fragment>
     </div>
   );
 };
+
 export default ShowCase;
