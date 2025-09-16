ede6f0c9ca3d869fde2354c3aa97307a48e0b4b3
yujiseok <koesijuy@gmail.com>
2025-09-16 10:22:24 +0900

    feat: 블러 데이터 상수로 변경

--- lib/blurUrl.ts
+++ lib/blurUrl.ts
@@ -1,7 +1,2 @@
-export const getBlurDataUrl = async (url: string) => {
-  const res = await fetch(url, {
-    cache: "no-store",
-  });
-  const buffer = await res.arrayBuffer();
-  return `data:image/png;base64,${Buffer.from(buffer).toString("base64")}`;
-};
+export const BLUR_DATA_URL =
+  "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q==";
